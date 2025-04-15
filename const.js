7726cf10926fc191e9ed0df7afd11db62e5ba28e
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-15 14:46:39 -0300

    Trabalhando com constantes

---
+++ const.js
@@ -0,0 +1,10 @@
+const SERIE = 'Friends';
+
+function x(params) {
+    const SERIE = 'The Walking Dead';
+    console.log('Dentro da função: ' + SERIE);
+}
+
+x();
+
+console.log('No escopo global: ' + SERIE);
