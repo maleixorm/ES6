6245901c53166e40b2e2c98604d89076c05adeb5
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-15 15:07:57 -0300

    Modificando arquivos

---
+++ var_let.js
@@ -0,0 +1,17 @@
+var serie = 'Friends';
+function x() {
+    var serie = 'Game of Thrones';
+    console.log(serie)
+    if (true) {
+        console.log(serie)
+    }
+}
+
+x();
+
+if (true) {
+    let serie = 'The Walking Dead';
+    console.log(serie);
+}
+
+console.log(serie);
