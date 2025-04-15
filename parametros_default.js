96a8fa88c289ba6ee27d18634605a6a0829c5e79
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-15 15:17:37 -0300

    utilizando parâmetros default nas funções

---
+++ parametros_default.js
@@ -0,0 +1,5 @@
+function contarUmaHistoria(cachorro = 'Fubá', atividade = 'passear', dono = 'Leleco') {
+    console.log(`Era uma vez um cachorro chamada(o) ${cachorro}. Ele adorava ${atividade}. Seu dono era ${dono} e eles viveram felizes para sempre!`);
+}
+
+contarUmaHistoria('Mel', 'Correr');
