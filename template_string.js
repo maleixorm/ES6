d4b9d7c37b45b0a34d27d0b0c55afb86c2c8b9f4
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-15 15:06:29 -0300

    Utilizando o template string no JS

---
+++ template_string.js
@@ -0,0 +1,10 @@
+let nome = 'Marcos';
+console.log(`
+    Oi ${nome}, tudo bem?
+`);
+
+function multiplica(x, y) {
+    return x * y;
+}
+
+console.log(`A multiplicação de 7 * 8 é igual a ${multiplica(7, 8)}.`);
