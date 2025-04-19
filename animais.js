b726d2086dda93310db659da295a133260847a41
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-18 23:46:59 -0300

    Trabalhando com herança no JS

--- animais.js
+++ animais.js
@@ -1,15 +1,21 @@
-class Cachorro {
-    constructor(cor, tamanho, orelhas) {
+class Animal {
+    constructor(cor, tamanho) {
         this.cor = cor;
         this.tamanho = tamanho;
+    }
+
+    dormir() {
+
+    }
+}
+
+class Cachorro extends Animal {
+    constructor(cor, tamanho, orelhas) {
+        super(cor, tamanho);
         this.orelhas = orelhas;
     }
 
     correr() {
-
-    }
-
-    dormir() {
 
     }
 
@@ -18,18 +24,19 @@ class Cachorro {
     }
 }
 
-class Passaro {
+class Passaro extends Animal  {
     constructor(cor, tamanho, bico) {
-        this.cor = cor;
-        this.tamanho = tamanho;
+        super(cor, tamanho);
         this.bico = bico;
     }
 
-    dormir() {
-
-    }
-
     voar() {
-        
+
     }
 }
+
+let cachorro = new Cachorro('Preto e Amarelo', 'Grande', 'Longas e Caídas');
+console.log(cachorro);
+
+let passaro = new Passaro('Branco e Azul', 'Médio', 'Pequenas');
+console.log(passaro);
