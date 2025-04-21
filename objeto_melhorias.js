3183ef77a4f86c35b2afad58f1bbabe3636976ba
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-21 20:03:11 -0300

    Aplicando melhorias em um novo objeto

--- objeto_melhorias.js
+++ objeto_melhorias.js
@@ -15,3 +15,16 @@ let objeto = {
 
 console.log(objeto);
 objeto.exibirResumo();
+
+let objeto2 = {
+    nome,
+    idade,
+    sexo,
+    profissao,
+    exibirResumo() {
+        console.log(`${this.nome}, ${this.idade}, ${this.sexo}, ${this.profissao}`);
+    }
+}
+
+console.log(objeto2);
+objeto2.exibirResumo();
