dfac8e702ee8e75002e0bfccc2c5cd58b26df0f2
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-21 17:52:11 -0300

    Criando um objeto literal

--- objeto_literal.js
+++ objeto_literal.js
@@ -11,3 +11,13 @@ class Produto {
 
 let produto = new Produto('Notebook', 2500);
 produto.verDescricao();
+
+let produtoLiteral = {
+    descricao: 'Iphone',
+    preco: 4500,
+    verDescricao: function() {
+        console.log(`${this.descricao} por apenas ${this.preco}`);
+    }
+}
+
+produtoLiteral.verDescricao();
