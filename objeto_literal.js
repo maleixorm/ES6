fee65bd1b7f1fd347e152855f5bf24490d55bfc5
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-21 17:23:29 -0300

    Trabalhando com objetos no JS

---
+++ objeto_literal.js
@@ -0,0 +1,13 @@
+class Produto {
+    constructor(descricao, preco) {
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    verDescricao() {
+        console.log(`${this.descricao} por apenas ${this.preco}`);
+    }
+}
+
+let produto = new Produto('Notebook', 2500);
+produto.verDescricao();
