afce93d58a9b30fc11ee087871cc62ef34b89b9f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-23 19:08:54 -0300

    Criando métodos na função construtora

--- funcao_construtora.js
+++ funcao_construtora.js
@@ -3,6 +3,23 @@ let Carro = function (cor, modelo, velocidadeAtual, velocidadeMaxima) {
     this.modelo = modelo;
     this.velocidadeAtual = velocidadeAtual;
     this.velocidadeMaxima = velocidadeMaxima;
+
+    this.acelerar = function(indice) {
+        let velocidade = this.getVelocidadeAtual() + indice;
+        this.setVelocidadeAtual(velocidade);
+    }
+
+    this.getVelocidadeAtual = function() {
+        return this.velocidadeAtual;
+    }
+
+    this.setVelocidadeAtual = function(velocidadeAtual) {
+        this.velocidadeAtual = velocidadeAtual;
+    }
 }
 
-let carro = new Carro();
+let carro = new Carro('Amarelo', 'Camaro', 0, 260);
+console.log(carro);
+
+carro.acelerar(100);
+console.log(carro);
