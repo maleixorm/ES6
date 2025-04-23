132a84d56c60381848c5c2862e14a71b64030fec
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-23 18:59:01 -0300

    Criando objeto a partir de função construtora

---
+++ funcao_construtora.js
@@ -0,0 +1,8 @@
+let Carro = function (cor, modelo, velocidadeAtual, velocidadeMaxima) {
+    this.cor = cor;
+    this.modelo = modelo;
+    this.velocidadeAtual = velocidadeAtual;
+    this.velocidadeMaxima = velocidadeMaxima;
+}
+
+let carro = new Carro();
