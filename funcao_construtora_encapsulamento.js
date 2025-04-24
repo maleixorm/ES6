346353b9d3d28a9053ec3b91783e03f248474225
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-23 23:50:13 -0300

    Criando atributos e métodos privados na função construtora

--- funcao_construtora_encapsulamento.js
+++ funcao_construtora_encapsulamento.js
@@ -1,15 +1,21 @@
-let Carro = function (cor, modelo, velocidadeAtual, velocidadeMaxima) {
+let Carro = function (cor, modelo, velocidadeAtual, vMax) {
+    
+    var velocidadeMaxima = vMax;
+    var quilometrosRodados = 0;
+    
     this.cor = cor;
     this.modelo = modelo;
     this.velocidadeAtual = velocidadeAtual;
-    this.velocidadeMaxima = velocidadeMaxima;
 
     this.acelerar = function(indice) {
         let velocidade = this.getVelocidadeAtual() + indice;
-        if (velocidade > this.velocidadeMaxima) {
-            velocidade = this.velocidadeMaxima;
+        if (velocidade > velocidadeMaxima) {
+            velocidade = velocidadeMaxima;
         }
         this.setVelocidadeAtual(velocidade);
+
+        setQuilometrosRodados(0.1);
+        console.log(quilometrosRodados);
     }
 
     this.getVelocidadeAtual = function() {
@@ -19,4 +25,21 @@ let Carro = function (cor, modelo, velocidadeAtual, velocidadeMaxima) {
     this.setVelocidadeAtual = function(velocidadeAtual) {
         this.velocidadeAtual = velocidadeAtual;
     }
+
+    var setQuilometrosRodados = function(q) {
+        quilometrosRodados += q;
+    }
 }
+
+let carro = new Carro('Vermelho', 'Ferrari', 0, 340);
+console.log(carro);
+carro.acelerar(10);
+console.log(carro.velocidadeAtual);
+carro.acelerar(10);
+console.log(carro.velocidadeAtual);
+carro.acelerar(10);
+console.log(carro.velocidadeAtual);
+carro.acelerar(10);
+console.log(carro.velocidadeAtual);
+carro.acelerar(10);
+console.log(carro.velocidadeAtual);
