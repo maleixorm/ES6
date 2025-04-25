0bb20eb63bc5c43b35234b71c7b6d43a299aa3f2
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-24 21:22:57 -0300

    Criando o objeto bicicleta

--- factory.js
+++ factory.js
@@ -1,1 +1,10 @@
 // Padrão de Projeto Factory
+
+let Bicicleta1 = {
+    cor: 'Branca',
+    marcha: 1,
+    aro: 12,
+    pedalar() { console.log('Método pedalar executado.'); }
+}
+
+console.log(Bicicleta1);
