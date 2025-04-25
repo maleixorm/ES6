b630d3dda5c32d1467aa0ebfbce8745c3ef81c8e
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-24 21:33:45 -0300

    Criando o objeto no modo Factory

--- factory.js
+++ factory.js
@@ -3,8 +3,31 @@
 let Bicicleta1 = {
     cor: 'Branca',
     marcha: 1,
-    aro: 12,
+    aro: 14,
+    pedalar() { console.log('Método pedalar executado.'); }
+}
+
+let Bicicleta2 = {
+    cor: 'Preta',
+    marcha: 18,
+    aro: 26,
     pedalar() { console.log('Método pedalar executado.'); }
 }
 
 console.log(Bicicleta1);
+console.log(Bicicleta2);
+
+let BicicletaFactory = function(cor, marcha, aro) {
+    return {
+        cor,
+        marcha,
+        aro,
+        pedalar() { console.log('Método pedalar executado.'); }
+    }
+}
+
+let Bicicleta3 = BicicletaFactory('Vermelha', 24, 30);
+console.log(Bicicleta3);
+
+let Bicicleta4 = BicicletaFactory('Azul', 21, 28);
+console.log(Bicicleta4);
