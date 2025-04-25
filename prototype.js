d22d2d6a80fb04cefadaa4812d10497b3635de3e
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-25 19:21:19 -0300

    Introdução a prototype no JS

---
+++ prototype.js
@@ -0,0 +1,31 @@
+// Objeto Literal
+let a1 = {
+    cor: 'Branca',
+    modelo: 'Airbus A380',
+    levantarVoo: function() { 
+        console.log('Levantar Voo.'); 
+    }
+}
+
+// Função Construtora
+let AviaoF = function(cor, modelo) {
+    this.cor = cor;
+    this.modelo = modelo;
+    this.levantarVoo = function() {
+        console.log('Levantar Voo.');
+    }
+}
+
+let a2 = new AviaoF('Azul', 'Boeing 747-800');
+
+// Classe
+class Aviao {
+    constructor(cor, modelo) {
+        this.cor = cor;
+        this.modelo = modelo;
+    }
+
+    levantarVoo() {
+        console.log('Levantar Voo.');
+    }
+}
