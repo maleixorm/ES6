c4fe51be54170d4873598029fd17e78e5833df7b
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-27 13:52:29 -0300

    Juntando valores com o rest

--- rest.js
+++ rest.js
@@ -1,7 +1,17 @@
 function soma(...param) {
     let resultado = 0;
-    param.forEach( v => resultado += v);
+    param.forEach(v => resultado += v);
     return resultado;
 }
 
 console.log(soma(3, 5, 7, 8, -10, 15, 120));
+
+function multiplicacao(m, ...p) {
+    console.log(m);
+    let resultado = 0;
+    console.log(p);
+    p.forEach(v => resultado += m*v);
+    return resultado;
+}
+
+console.log(multiplicacao(5, 7, 12, 3, 49));
