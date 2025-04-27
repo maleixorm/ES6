e9cae2a4a31177e0be5a21d396a402cfbf0e5f88
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-26 21:29:10 -0300

    Acessando protótipos em cadeia

---
+++ prototype_chain.js
@@ -0,0 +1,17 @@
+let animal = {
+    attr1: 'a'
+}
+
+let vertebrado = {
+    __proto__: animal,
+    attr2: 'b'
+}
+
+let ave = {
+    __proto__: vertebrado,
+    attr3: 'c'
+}
+
+console.log(ave.attr1);
+console.log(ave.attr2);
+console.log(ave.attr3);
