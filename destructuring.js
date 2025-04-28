716c224f2b84d49360bafbef1dac0f8f7413995b
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-04-28 18:49:06 -0300

    Desestruturando vetores dentro de vetores

--- destructuring.js
+++ destructuring.js
@@ -1,3 +1,7 @@
 let frutas = ['abacaxi', 'uva', 'pera', 'mamao'];
-let [a, b, c, d] = frutas;
-console.log(a, b, c, d);
+let [a, b, c, d, e = 'banana'] = frutas;
+console.log(a, b, c, d, e);
+
+let coisas = [['Carne', 'Frutas', 'Verduras', 'legumes'], ['José', 'Maria']];
+let [,[,n2]] = coisas;
+console.log(n2);
